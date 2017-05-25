0ad40c15cd1cfd5008edac2f050dc0cf7eae9d83
Matt Martineau <mm14628@bristol.ac.uk>
2017-05-25 13:49:43 +0100

    Added a least squares approximation to the cell centered gradients for the guessed component

--- omp3/nodes.c
+++ omp3/nodes.c
@@ -68,46 +68,6 @@ double initialise_cg(
   // density or mesh were changed by another package, that the coefficients
   // are updated accordingly, making performance evaluation fairer.
   
-  for(int ii = PAD; ii < ny-PAD; ++ii) {
-    for(int jj = PAD; jj < nx-PAD; ++jj) {
-      const int cell_index = (ii)*nx+(jj);
-      double k = 0.0;
-      
-      // TODO: In the future nedges will be dependent upon cell.
-      for(int ff = 0; ff < nedges; ++ff) {
-        const int neighbour_index = neighbours[(ff)*nx*ny+(cell_index)];
-        const double density0 = rho[(cell_index)];
-        const double density1 = rho[(neighbour_index)];
-
-        // Fetch the vertices for the current edge
-        const int edge_index = cells_edges[(ff)*nx*ny+(cell_index)];
-        const int vertex0 = edge_vertex0[(edge_index)];
-        const int vertex1 = edge_vertex1[(edge_index)];
-
-        // Calculate the distance between centroids
-        const double cell_centroid_x = cell_centroids_x[(cell_index)];
-        const double cell_centroid_y = cell_centroids_y[(cell_index)];
-        const double neighbour_centroid_x = cell_centroids_x[(neighbour_index)];
-        const double neighbour_centroid_y = cell_centroids_y[(neighbour_index)];
-        const double cell_dx = (neighbour_centroid_x-cell_centroid_x);
-        const double cell_dy = (neighbour_centroid_y-cell_centroid_y);
-        const double ds = sqrt(cell_dx*cell_dx+cell_dy*cell_dy);
-
-        // Calculate the unit vector joining cell centroids
-        const double es_x = cell_dx/ds;
-        const double es_y = cell_dy/ds;
-
-        // Calculate the area vector
-        const double A_x = -(vertices_x[vertex1]-vertices_x[vertex0]);
-        const double A_y = vertices_y[vertex1]-vertices_y[vertex0];
-
-        const double edge_density = (2.0*density0*density1)/(density0+density1);
-        const double diffusion_coeff = conductivity/(edge_density*heat_capacity);
-
-      }
-    }
-  }
-
   // https://inldigitallibrary.inl.gov/sti/3952796.pdf
   // Take the average of the coefficients at the cells surrounding 
   // each edge
@@ -147,6 +107,103 @@ double initialise_cg(
 
   STOP_PROFILING(&compute_profile, "initialise cg");
   return initial_r2;
+}
+
+
+void calculate_unstructured_correction(
+    const int nx, const int ny, const int nedges, const double heat_capacity,
+    const double conductivity, const double* volume, const double* rho, 
+    const double* temperature,
+    const int* edge_vertex0, const int* edge_vertex1,
+    const int* cell_centroids_x, const int* cell_centroids_y,
+    const int* vertices_x, const int* vertices_y,
+    const int* cells_edges, const int* neighbours)
+{
+  // Find the gradient for all cells
+  for(int ii = PAD; ii < ny-PAD; ++ii) {
+    for(int jj = PAD; jj < nx-PAD; ++jj) {
+      const int cell_index = (ii)*nx+(jj);
+      double k = 0.0;
+
+      // Fetch the volume of the cell
+      double V = volume[cell_index];
+
+      /*
+       *    Performing least squares approximation to get unknown d
+       *    d = [ dphi/dx, dphi/dy ]
+       *    M = [ (dx0, dx1 ..., dx_ff) (dy0, dy1, ..., dy_ff) ]
+       *    del(phi) = [ phi1-phi0, phi2-phi0, ..., phi_ff-phi0 ]
+       *    d = (M^T.M)^(-1).(M^T).del(phi)
+       */
+
+      // Calculate the coefficents to matrix M
+      double MTM[3] = { 0.0 }; // Describes the three unique quantities in (M^T.M)
+      double MT_del_phi[2] = { 0.0 };
+
+      // Calculate the coefficients for all edges
+      for(int ff = 0; ff < nedges; ++ff) {
+        const int neighbour_index = neighbours[(ff)*nx*ny+(cell_index)];
+
+        // Calculate the distance between centroids
+        const double cell_centroid_x = cell_centroids_x[(cell_index)];
+        const double cell_centroid_y = cell_centroids_y[(cell_index)];
+        const double neighbour_centroid_x = cell_centroids_x[(neighbour_index)];
+        const double neighbour_centroid_y = cell_centroids_y[(neighbour_index)];
+
+        // Calculate the cell differentials
+        const double cell_dx = (neighbour_centroid_x-cell_centroid_x);
+        const double cell_dy = (neighbour_centroid_y-cell_centroid_y);
+
+        const double phi0 = temperature[(cell_index)];
+        const double phi_ff = temperature[(neighbour_index)];
+        MTM[0] += cell_dx*cell_dx;
+        MTM[1] += cell_dx*cell_dy;
+        MTM[2] += cell_dy*cell_dy;
+        MT_del_phi[0] += cell_dx*(phi_ff-phi0);
+        MT_del_phi[1] += cell_dy*(phi_ff-phi0);
+      }
+
+      const double MTM_det = (1.0/(MTM[0]*MTM[2]-MTM[1]*MTM[1]));
+      temp_grad_cell_x[(cell_index)] = 
+        MTM_det*(MT_del_phi[0]*MTM[2]-MT_del_phi[1]*MTM[1]);
+      temp_grad_cell_y[(cell_index)] = 
+        MTM_det*(MT_del_phi[1]*MTM[0]-MT_del_phi[0]*MTM[1]);
+
+#if 0
+        const double density0 = rho[(cell_index)];
+        const double density1 = rho[(neighbour_index)];
+
+        // Calculate the edge differentials
+        const int edge_index = cells_edges[(ff)*nx*ny+(cell_index)];
+        const int vertex0 = edge_vertex0[(edge_index)];
+        const int vertex1 = edge_vertex1[(edge_index)];
+        const double edge_dx = (vertices_x[vertex1]-vertices_x[vertex0]);
+        const double edge_dy = (vertices_y[vertex1]-vertices_y[vertex0]);
+
+        // Calculate the centroid distance and length of edge
+        const double centroid_distance = sqrt(cell_dx*cell_dx+cell_dy*cell_dy);
+        const double edge_length = sqrt(edge_dx*edge_dx+edge_dy*edge_dy);
+
+        // Calculate the unit vector joining cell centroids
+        const double es_x = cell_dx/centroid_distance;
+        const double es_y = cell_dy/centroid_distance;
+
+        // Calculate the unit vector joining the vertices
+        const double et_x = edge_dx/edge_length;
+        const double et_y = edge_dy/edge_length;
+
+        // Calculate the area vector
+        const double A_x = edge_dy;
+        const double A_y = -edge_dx;
+
+#endif // if 0
+
+        const double edge_density = (2.0*density0*density1)/(density0+density1);
+        const double diffusion_coeff = conductivity/(edge_density*heat_capacity);
+
+      }
+    }
+  }
 }
 
 // Calculates a value for alpha
